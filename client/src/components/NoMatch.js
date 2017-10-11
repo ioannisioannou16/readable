b17f9f63eb74abe4157d5543fc5a694e2ad54675
Ioannis Ioannou <ioannisioannou16@gmail.com>
2017-10-11 20:08:02 +0100

    improved notfound page

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from "reactstrap"
 
-const NoMatch = ({ location }) => (
+const goHome = (history) => () => history.push('/')
+
+const goBack = (history) => () => history.goBack()
+
+const NoMatch = ({ location, history }) => (
   <div className="centered">
-    <h1 className="text-center">Page not found</h1>
+    <h1 className="text-center mb-5">Page not found</h1>
+    <Button color="link" onClick={goBack(history)}>Go back</Button>
+    <Button color="link" className="pull-right" onClick={goHome(history)}>Go home</Button>
   </div>
 )
 
